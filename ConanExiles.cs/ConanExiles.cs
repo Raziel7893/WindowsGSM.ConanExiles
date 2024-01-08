cdec6bf8bcde719c3cf2bd0cb83464c7aa67d63a
Soulflare3 <robert@sf3soft.net>
2024-01-08 18:30:52 -0500

    Converted internal plugin to external

---
+++ ConanExiles.cs/ConanExiles.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Text;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using WindowsGSM.Functions;
+using WindowsGSM.GameServer.Engine;
+using WindowsGSM.GameServer.Query;
+using System.IO;
+
+namespace WindowsGSM.Plugins
+{
+    public class ConanExiles : SteamCMDAgent
+    {
+        // - Plugin Details
+        public Plugin Plugin = new Plugin
+        {
+            name = "WindowsGSM.ConanExiles", // WindowsGSM.XXXX
+            author = "Soul",
+            description = "\U0001f9e9 A plugin version of the Vintage Story Dedicated server for WindowsGSM",
+            version = "1.0",
+            url = "https://github.com/Soulflare3/WindowsGSM.ConanExiles", // Github repository link (Best practice)
+            color = "#7a0101" // Color Hex
+        };
+
+        // - Standard Constructor and properties
+        public ConanExiles(ServerConfig serverData) : base(serverData) => base.serverData = _serverData = serverData;
+        private readonly ServerConfig _serverData;
+
+        // - Settings properties for SteamCMD installer
+        public override bool loginAnonymous => true;
+        public override string AppId => "443030";
+
+        // - Game server Fixed variables
+        public override string StartPath => @"ConanSandbox\Binaries\Win64\ConanSandboxServer-Win64-Shipping.exe";
+        public string FullName = "Conan Exiles Dedicated Server";
+        public bool AllowsEmbedConsole = true;
+        public int PortIncrements = 2;
+        public object QueryMethod = new A2S();
+
+        // - Game server default values
+        public string Port = "7777";
+        public string ServerName = "Conan Exiles Dedicated Server";
+        public string QueryPort = "27015";
+        public string Defaultmap = "game.db";
+        public string Maxplayers = "40";
+        public string Additional = "";
+
+
+        // - Create a default cfg for the game server after installation
+        public async void CreateServerCFG()
+        {
+            //Download Engine.ini
+            string configPath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, @"ConanSandbox\Saved\Config\WindowsServer\Engine.ini");
+            if (await Functions.Github.DownloadGameServerConfig(configPath, FullName))
+            {
+                string configText = File.ReadAllText(configPath);
+                configText = configText.Replace("{{ServerName}}", _serverData.ServerName);
+                configText = configText.Replace("{{ServerPassword}}", _serverData.GetRCONPassword());
+                File.WriteAllText(configPath, configText);
+            }
+        }
+
+        public async Task<Process> Start()
+        {
+            string shipExePath = Functions.ServerPath.GetServersServerFiles(_serverData.ServerID, StartPath);
+            if (!File.Exists(shipExePath))
+            {
+                Error = $"{Path.GetFileName(shipExePath)} not found ({shipExePath})";
+                return null;
+            }
+
+            string param = string.IsNullOrWhiteSpace(_serverData.ServerIP) ? string.Empty : $" -MultiHome={_serverData.ServerIP}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerPort) ? string.Empty : $" -Port={_serverData.ServerPort}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerQueryPort) ? string.Empty : $" -QueryPort={_serverData.ServerQueryPort}";
+            param += string.IsNullOrWhiteSpace(_serverData.ServerMaxPlayer) ? string.Empty : $" -MaxPlayers={_serverData.ServerMaxPlayer}";
+            param += $" {_serverData.ServerParam}" + (!AllowsEmbedConsole ? " -log" : string.Empty);
+
+            Process p;
+            if (!AllowsEmbedConsole)
+            {
+                p = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = shipExePath,
+                        Arguments = param,
+                        WindowStyle = ProcessWindowStyle.Minimized,
+                        UseShellExecute = false
+                    },
+                    EnableRaisingEvents = true
+                };
+                p.Start();
+            }
+            else
+            {
+                p = new Process
+                {
+                    StartInfo =
+                    {
+                        FileName = shipExePath,
+                        Arguments = param,
+                        WindowStyle = ProcessWindowStyle.Hidden,
+                        CreateNoWindow = true,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true
+                    },
+                    EnableRaisingEvents = true
+                };
+                var serverConsole = new Functions.ServerConsole(_serverData.ServerID);
+                p.OutputDataReceived += serverConsole.AddOutput;
+                p.ErrorDataReceived += serverConsole.AddOutput;
+                p.Start();
+                p.BeginOutputReadLine();
+                p.BeginErrorReadLine();
+            }
+
+            return p;
+        }
+
+        public async Task Stop(Process p)
+        {
+            await Task.Run(() =>
+            {
+                if (p.StartInfo.CreateNoWindow)
+                {
+                    p.CloseMainWindow();
+                }
+                else
+                {
+                    Functions.ServerConsole.SetMainWindow(p.MainWindowHandle);
+                    Functions.ServerConsole.SendWaitToMainWindow("^c");
+                }
+            });
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# WindowsGSM.ConanExiles
-A plugin version of the Conan Exiles Dedicated server for WindowsGSM 
+﻿# WindowsGSM.ConanExiles
+🧩 A plugin version of the Conan Exiles Dedicated server for WindowsGSM
+
+## Requirements
+[WindowsGSM](https://github.com/WindowsGSM/WindowsGSM) >= 1.21.0
+
+## Installation
+1. Download the [latest](https://github.com/Soulflare3/WindowsGSM.ConanExiles/releases/latest) release
+1. Move **ConanExiles.cs** folder to **plugins** folder or import the zip from the plugins tab
+1. Click the **[RELOAD PLUGINS]** button or restart WindowsGSM
+
+### License
+This project is licensed under the MIT License - see the [LICENSE.md](https://github.com/Soulflare3/WindowsGSM.ConanExiles/blob/master/LICENSE) file for details
